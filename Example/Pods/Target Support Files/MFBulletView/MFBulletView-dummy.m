c187f24cfe3db4851d85c17e94e65aa12cf5c383
NealWills <NealWills93@gmail.com>
2022-08-15 17:44:29 +0800

    0.0.1 test

---
+++ Example/Pods/Target Support Files/MFBulletView/MFBulletView-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_MFBulletView : NSObject
+@end
+@implementation PodsDummy_MFBulletView
+@end
---
+++ Example/Pods/Target Support Files/Pods-MFBulletView_Example/Pods-MFBulletView_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_MFBulletView_Example : NSObject
+@end
+@implementation PodsDummy_Pods_MFBulletView_Example
+@end
