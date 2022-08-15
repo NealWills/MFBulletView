b926990bf93bb48972dd16bace026cf509c604d4
NealWills <NealWills93@gmail.com>
2022-08-15 17:42:30 +0800

    Initial commit

---
+++ Example/MFBulletView/MFViewController.h
@@ -0,0 +1,13 @@
+//
+//  MFViewController.h
+//  MFBulletView
+//
+//  Created by NealWills on 08/15/2022.
+//  Copyright (c) 2022 NealWills. All rights reserved.
+//
+
+@import UIKit;
+
+@interface MFViewController : UIViewController
+
+@end
---
+++ Example/MFBulletView/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  MFBulletView
+//
+//  Created by NealWills on 08/15/2022.
+//  Copyright (c) 2022 NealWills. All rights reserved.
+//
+
+@import UIKit;
+#import "MFAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([MFAppDelegate class]));
+    }
+}
